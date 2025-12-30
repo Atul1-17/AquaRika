a1aee252fec87c9e4787bcaa2758f0a0a947ec69
Atul Bopche <atul962355@gmail.com>
2025-12-31 00:08:53 +0530

    Enhance Banner and HeroSection components with improved layout, styling, and content for better user engagement

--- src/components/sheared/Banner.jsx
+++ src/components/sheared/Banner.jsx
@@ -22,10 +22,24 @@ function Banner() {
 
     {/* mobile screen */}
 
-    <div className='bg-[#9bc6d1] h-[20vh] w-full flex md:hidden'>
+    <div className='h-[40vh] md:hidden flex flex-col gap-4'>
+      <div className='bg-[#9bc6d1] h-[20vh] relative w-full flex'>
         <div className='w-[60%] rounded-tr-full bg-gray-100'>
         </div>
-        <div className='absolute'></div>
+        <div className='absolute h-[20vh] w-full flex items-center justify-center text-center'>
+          <h2 className='font-medium text-xl'>with great power comes great responsibility</h2>
+        </div>
+      </div>
+      <div className='p-4 text-center'>
+        <p>
+          It is our mission to be the country's 
+          premium refreshment product 
+          company, focused on delivering a 
+          family of flavourful, great-tasting 
+          and optimum beverages that 
+          delight the customer.
+        </p>
+      </div>
     </div>
     </>
   )
--- src/components/sheared/HeroSection.jsx
+++ src/components/sheared/HeroSection.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 function HeroSection() {
   return (
-    <div className='h-[90vh] relative w-full flex flex-col md:flex-row lg:flex-row'>
+    <div className='h-[90vh] relative w-full flex flex-col md:flex-row lg:flex-row gap-4 md:gap-0'>
       <div className='w-[100%] h-[50%] md:w-[50%] md:h-[100%] lg:w-[50%] lg:h-[100%] flex items-center justify-center'>
         <div className=' flex flex-col items-start p-4 gap-4 justify-center'>
-          <h1 className='text-4xl md:text-6xl lg:text-6xl  leading-normal'>Reinventing the Way<br/> You Drink Water</h1>
-          <p>Because Every Sip Should Be Perfect</p>
-          <button className='hidden md:flex lg:flex border-2 px-4 py-2 bg-[#1e8bb0] text-white rounded-3xl'>Contact Us</button>
+          <h1 className='text-5xl md:text-6xl lg:text-7xl font-medium  lg:leading-normal'>Reinventing the Way<br/> You Drink Water</h1>
+          <p className='lg:text-2xl font-light'>Because Every Sip Should Be Perfect</p>
+          <button className='hidden md:flex lg:flex border-2 px-15 py-4 bg-[#1e8bb0] text-xl text-white rounded-3xl'>Contact Us</button>
         </div>
       </div>
       <div className='relative w-[100%] h-[100%] md:w-[50%] md:h-[100%] lg:w-[50%] lg:h-[100%]'>
-        <div className='h-[80%] w-[65%] md:h-[80%] md:w-[50%] lg:h-[80%] lg:w-[50%] rounded-t-full absolute top-[0%] right-[10%] md:top-[5%] md:right-[10%] border-2'>
+        <div className=' h-[70%] w-[65%] md:h-[80%] md:w-[50%] lg:h-[80%] lg:w-[50%] rounded-t-full absolute top-[0%] right-[10%] md:top-[5%] md:right-[10%] border-2'>
           <img className='h-[100%] w-[100%] rounded-t-full' src="/Gemini_Generated_Image_3y493y493y493y49.png" alt="" />
         </div>
-        <div className='rounded-full h-40 w-40 md:h-50 md:w-50 lg:h-50 lg:w-50 bg-red-500 absolute bottom-[15%] left-[10%] md:bottom-[10%] md:left-[30%] border-2'>
+        <div className=' rounded-full h-40 w-40 md:h-50 md:w-50 lg:h-50 lg:w-50 bg-red-500 absolute bottom-[20%] left-[10%] md:bottom-[10%] md:left-[30%] border-2'>
           <img src="" alt="" />
         </div>
-        <button className='flex absolute bottom-[1%] right-[30%] md:hidden lg:hidden border-2 px-4 py-2 bg-[#1e8bb0] text-white rounded-3xl'>Contact Us</button>
+        <button className='flex absolute bottom-[1%] right-[30%] md:hidden lg:hidden border-2 px-15 py-4 bg-[#1e8bb0] text-white rounded-3xl'>Contact Us</button>
       </div>
     </div>
   )
